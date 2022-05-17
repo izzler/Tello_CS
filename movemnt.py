6278183bafbd8371d07a1fba621c134fdb6be362
izzler <105304986+izzler@users.noreply.github.com>
2022-05-17 08:18:12 -0700

    Update movemnt.py

--- movemnt.py
+++ movemnt.py
@@ -1,8 +1,35 @@
 from djitellopy import tello
 from time import sleep
 
-me = tello.Tello()
-me.connect()
-
-print(me.get_battery())
- 
+babner = tello.Tello()
+babner.connect()
+print(babner.get_battery())
+# take off at 170cm
+babner.takeoff()
+babner.moveup(120)
+# move forward 150cm
+babner.move_forward(310)
+sleep(1)
+# turn
+babner.rotate_clockwise(90)
+sleep(1)
+# move forward 50cm
+babner.move_forward(600)
+sleep(1)
+# turn
+babner.rotate_clockwise(90)
+sleep(1)
+# cool slip
+babner.flip()
+sleep(1)
+# move forward 150cm
+babner.move_forward(310)
+sleep(1)
+# turn
+babner.rotate_clockwise(90)
+sleep(1)
+# move forward
+babner.move_forward(600)
+sleep(1)
+# landing
+babner.land()
